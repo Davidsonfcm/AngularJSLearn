e8dda13007b2be8c64a2d6b50917a0ca71860f91
Davidson Felipe Caetano de Morais <davidson.morais@webaula.local>
2017-03-27 12:35:48 -0300

    Meu primeiro Commit

---
+++ app/events/event-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from 'angular2/core';
+import { IEvent } from './event';
+import { EventFilterPipe } from './event-filter.pipe';
+import { ThumbComponent } from '../shared/thumb.component';
+import { EventService } from './event-service';
+
+
+@Component({
+    selector: 'el-events',
+    templateUrl: 'app/events/event-list.component.html',
+    styleUrls: ['app/events/event-list.component.css'],
+    pipes: [EventFilterPipe],
+    directives: [ThumbComponent]
+})
+
+export class EventListComponent implements OnInit {
+    pageTitle: string = '+ Lista de Eventos +';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    searchCriteria: string;
+    events: IEvent[];
+
+    constructor(private _eventService:  EventService) {   }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+    ngOnInit(): void  {
+        this.events = this._eventService.getEvents();
+    }
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Event List: ' + message;
+    }
+}
---
+++ app/events/event.js
@@ -0,0 +1,31 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[],
+        execute: function() {
+        }
+    }
+});
+/*export class Event implements IEvent {
+    
+    constructor(
+        public name: string,
+        public code: string,
+        public description: string,
+        public date: string,
+        public time: string,
+        public duration: string,
+        public fee: number,
+        public rating: number,
+        public imageUrl: string,
+        public location: Object,
+        public capacity: number){
+        }
+        
+    calculateDiscount(percent: number) : number {
+        return this.price - (this.price * percent / 100)
+    }
+}*/
+
+//# sourceMappingURL=event.js.map
